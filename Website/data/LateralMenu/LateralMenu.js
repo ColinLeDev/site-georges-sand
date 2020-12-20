b3da5d2a22fee8af4bede2721c4997bfe2cd06d0
Colin <46822888+ColinLeDev@users.noreply.github.com>
2020-12-20 03:50:59 +0100

    Upload V0.1 site
    Base script et menu de nav.
    présence du scripts.html, lieu de stockage des scripts utilisés

---
+++ Website/data/LateralMenu/LateralMenu.js
@@ -0,0 +1,9 @@
+/* Mettre la largeur du lat menu à 250px (ouvrir) */
+function openNavMenu() {
+    document.getElementById("LatMenu").style.width = "250px";
+  }
+  
+  /* Mettre la largeur à 0 (fermer du coup) */
+  function closeNavMenu() {
+    document.getElementById("LatMenu").style.width = "0";
+  }
